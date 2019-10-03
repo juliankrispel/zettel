74b16c11c4d0a5c47a487a544a25929837be461c
Julian Krispel-Samsel <julian@reactrocket.com>
2019-10-03 19:15:56 +0100

    updates

--- experiments/src/examples/MarkdownTest/index.tsx
+++ experiments/src/examples/MarkdownTest/index.tsx
@@ -19,22 +19,6 @@ const App = () => {
         const { htmlAttrs, children, block } = props
         const text = block.fragments.map(frag => frag.text).join('')
 
-        if (text.startsWith('# ')) {
-          return <h1 {...htmlAttrs}>{children}</h1>
-        }
-
-        if (text.startsWith('## ')) {
-          return <h2 {...htmlAttrs}>{children}</h2>
-        }
-
-        if (text.startsWith('### ')) {
-          return <h3 {...htmlAttrs}>{children}</h3>
-        }
-
-        if (text === '---') {
-          return <hr />
-        }
-
         return <div {...htmlAttrs}>{children}</div>
       }}
       editorState={editorState}
