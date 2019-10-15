67479de32a6efce7145d16e36c4fc4877d4b1fec
Julian Krispel-Samsel <julian@reactrocket.com>
2019-10-15 16:04:50 +0100

    cleanup files

--- docs/next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-const withTypescript = require('@zeit/next-typescript')
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/
-})
-
-module.exports = withTypescript(withMDX({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx']
-}))
