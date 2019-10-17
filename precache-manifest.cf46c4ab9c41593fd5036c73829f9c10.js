0ed6c62a769006c4e4b8a01c5b52ade4f17223a4
Julian Krispel-Samsel <julian@reactrocket.com>
2019-10-17 20:20:59 +0100

    Updates

---
+++ precache-manifest.cf46c4ab9c41593fd5036c73829f9c10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331811db929e12c4985befb14e3d5f40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d02df76f4bb6c1f401d",
+    "url": "/static/css/main.55055bad.chunk.css"
+  },
+  {
+    "revision": "13c625bb16da794abe51",
+    "url": "/static/js/2.cb0e609c.chunk.js"
+  },
+  {
+    "revision": "0d02df76f4bb6c1f401d",
+    "url": "/static/js/main.42d713ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
