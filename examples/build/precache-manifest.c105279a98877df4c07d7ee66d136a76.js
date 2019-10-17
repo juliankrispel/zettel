b8fcc52d699fc6bbb8c64a4c3a42711b224285f4
Julian Krispel-Samsel <julian@reactrocket.com>
2019-10-17 17:09:28 +0100

    update packages to fix deps

---
+++ examples/build/precache-manifest.c105279a98877df4c07d7ee66d136a76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2019430b3d780d522b5e885259ed08a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38b412b148f019713269",
+    "url": "/static/css/main.5167444e.chunk.css"
+  },
+  {
+    "revision": "e9cf5665548704eabd2d",
+    "url": "/static/js/2.884a1313.chunk.js"
+  },
+  {
+    "revision": "38b412b148f019713269",
+    "url": "/static/js/main.654cfcf6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- examples/src/examples/Rtl.tsx
+++ examples/src/examples/Rtl.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { EditorState } from '@zettel/core'
 import Editor from '@zettel/react'
 
-const text = `[One 😅Line][And another line of text][And another line]`
+const text = `[One 😅Line][يثصضنيهتثتتادويزرعقتبت][And another line]`
 
 const App = () => {
   const [editorState, setEditorState] = useState(() => EditorState.fromJSON({
