2c296228db7e92e9df937f31821699932cef22d8
Julian Krispel-Samsel <julian@reactrocket.com>
2020-05-18 15:42:29 +0100

    update release

--- scripts/release.js
+++ scripts/release.js
@@ -1,11 +1,22 @@
 const parse = require('changelog-parser')
 const core = require('@actions/core')
+const github = require('@actions/github');
+const fs = require('fs')
+const path = require('path')
+const packages = process.env.PACKAGES.split(',').map(package => package.trim())
 
-parse('./CHANGELOG.md')
-.then(log => {
+async function run() {
+  const log = await parse('./CHANGELOG.md')
+
   const version = log.versions.find(version => version.version != null)
-  console.log('huh?', core.setOutput('title', version.title))
+  core.setOutput('title', version.title)
   core.setOutput('version', version.version)
   core.setOutput('description', version.body)
-  console.log('did this work?', version)
-})
+
+  packages.forEach(package => {
+    const packageJson = JSON.parse(fs.readFileSync(path.join('./', package, 'package.json')).toString())
+    if (packageJson.version !== version.version) {
+      core.setFailed(`Release failed because version in changelog: ${version.version} isn't the same as version in ${packageJson.name} which is ${packageJson.version}`)
+    }
+  })
+}
