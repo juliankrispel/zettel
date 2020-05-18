a9b889a8dff29d43b5860773ad2ad9e9e9aa51cb
Julian Krispel-Samsel <julian@reactrocket.com>
2020-05-18 16:06:19 +0100

    update releease workflow

--- scripts/release.js
+++ scripts/release.js
@@ -4,14 +4,27 @@ const github = require('@actions/github');
 const fs = require('fs')
 const path = require('path')
 const packages = process.env.PACKAGES.split(',').map(package => package.trim())
+const token = process.env.GITHUB_TOKEN
+
+const octokit = new github.GitHub(token);
+
 
 async function run() {
   const log = await parse('./CHANGELOG.md')
+
+
+  const { data: releases } = await octokit.repos.listReleases({
+    owner: 'juliankrispel',
+    repo: 'zettel'
+  })
+  console.log({ releases })
 
   const version = log.versions.find(version => version.version != null)
   core.setOutput('title', version.title)
   core.setOutput('version', version.version)
   core.setOutput('description', version.body)
+  core.setOutput('already_released', false)
+
 
   packages.forEach(package => {
     const packageJson = JSON.parse(fs.readFileSync(path.join('./', package, 'package.json')).toString())
@@ -19,4 +32,14 @@ async function run() {
       core.setFailed(`Release failed because version in changelog: ${version.version} isn't the same as version in ${packageJson.name} which is ${packageJson.version}`)
     }
   })
+
+  // check if already released on github
+  releases.forEach(release => {
+    if(release.tag_name.includes(version.version)) {
+      core.setFailed(`Release failed because version in changelog: ${version.version} isn't the same as version in ${packageJson.name} which is ${packageJson.version}`)
+      core.setOutput('already_released', true)
+    }
+  })
 }
+
+run()
