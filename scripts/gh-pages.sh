8c920f04d96119937fbbb4c291a6397d954b25bb
Julian Krispel-Samsel <julian@reactrocket.com>
2020-05-18 19:27:40 +0100

    remove bash scripts, add gh-pages to release workflow

--- scripts/gh-pages.sh
+++
@@ -1,6 +0,0 @@
-cd site
-yarn
-yarn build
-cp build/index.html build/404.html 
-cd ..
-yarn gh-pages -d ./examples/build
--- scripts/publish-all.sh
+++
@@ -1,9 +0,0 @@
-
-echo "publish @zettel/core"
-cd core
-npm publish
-cd ..
-echo "publish @zettel/react"
-cd react
-npm publish
-cd ..
