2c9d2842f3f66f6f3d1f98fee3734e8f0e3ffdd7
Julian Krispel-Samsel <julian@reactrocket.com>
2020-05-18 14:25:56 +0100

    again

--- scripts/release.js
+++ scripts/release.js
@@ -5,7 +5,6 @@ parse('./CHANGELOG.md')
 .then(log => {
   const version = log.versions.find(version => version.version != null)
   console.log('huh?', core.setOutput('title', version.title))
-
   core.setOutput('version', version.version)
   core.setOutput('description', version.body)
   console.log('did this work?', version)
