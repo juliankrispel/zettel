4f2bb2d107dc8f0b01906fab5253db6f666de103
Julian Krispel-Samsel <julian@reactrocket.com>
2020-06-01 21:13:46 +0100

    update reference and type

--- core/src/selection/getFragmentOffset.ts
+++ core/src/selection/getFragmentOffset.ts
@@ -1,27 +1,6 @@
 import { Value } from '../types'
 import getBlockOffset from '../query/getBlockOffset'
-
-interface ElementWithDataSet extends HTMLElement {
-  readonly dataset: {
-    fragmentStart: string,
-    blockKey: string
-  }
-}
-
-const getFragmentNode = (el: HTMLElement | null): ElementWithDataSet | null => {
-  if (el == null) {
-    return null
-  }
-
-  if (el.dataset && el.dataset.blockKey != null && el.dataset.fragmentStart != null) {
-    const _el: any = el
-    return _el
-  } else if (el.parentElement) {
-    return getFragmentNode(el.parentElement)
-  }
-
-  return null
-}
+import getFragmentNode from './getFragmentNode'
 
 export default function getFragmentOffset (value: Value, node: HTMLElement) {
   const fragment = getFragmentNode(node)
